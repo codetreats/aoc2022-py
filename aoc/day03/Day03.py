afb7001c17ae52af3211846e459a19d23e86ba0b
Martin <github@msturm.net>
2022-12-03 08:45:45 +0100

    Add day03 part 2

--- aoc/day03/Day03.py
+++ aoc/day03/Day03.py
@@ -14,18 +14,24 @@ class Day03(Day):
             half = int(len(line) / 2)
             first = line[:half]
             second = line [half:]
-            line_items = ""
             for f in first:
-                if f in second and f not in line_items:
-                    line_items += f
-            items += line_items    
+                if f in second:
+                    items += f
+                    break
         return sum([self.get_value(c) for c in items])
 
     def run2(self):
-        return ""
+        badges = ""
+        for index in range(0, len(self.data), 3):
+            for f in self.data[index]:
+                if f in self.data[index + 1] and f in self.data[index + 2]:
+                    badges += f
+                    break
+        return sum([self.get_value(c) for c in badges])
 
     def get_value(self, string):
         val = ord(string)
         if val > 96:
             return val - 96
         return val - 38
+
--- test/test_Day03.py
+++ test/test_Day03.py
@@ -5,19 +5,19 @@ from aoc.day03.Day03 import Day03
 class testDay03(unittest.TestCase):
     def test_part1_dummy(self):
         class_under_test = Day03().init_with(True)
-        self.assertEqual("", str(class_under_test.run1()))
+        self.assertEqual("157", str(class_under_test.run1()))
 
     def test_part1(self):
         class_under_test = Day03().init_with(False)
-        self.assertEqual("", str(class_under_test.run1()))
+        self.assertEqual("7446", str(class_under_test.run1()))
 
     def test_part2_dummy(self):
         class_under_test = Day03().init_with(True)
-        self.assertEqual("", str(class_under_test.run2()))
+        self.assertEqual("70", str(class_under_test.run2()))
 
     def test_part2(self):
         class_under_test = Day03().init_with(False)
-        self.assertEqual("", str(class_under_test.run2()))
+        self.assertEqual("2646", str(class_under_test.run2()))
 
 
 if __name__ == '__main__':
