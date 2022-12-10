d6843782341080d17e8cd59d2e81fba2fefb4907
Martin <github@msturm.net>
2022-12-10 11:02:13 +0100

    Add day10 part1

--- aoc/day10/Day10.py
+++ aoc/day10/Day10.py
@@ -6,10 +6,25 @@ class Day10(Day):
     use_dummy = True
 
     def convert(self, lines):
-        return lines
+        ops = [ None ]
+        for line in lines:
+            ops.append(None)
+            if line != "noop":
+                ops.append(int(line.split()[1]))
+        return ops
 
     def run1(self):
-        return ""
+        x = 1
+        strength = 0
+        for i in range(1, 221):
+            if i == 20 or i == 60 or i == 100  or i == 140  or i == 180  or i == 220:
+                print(str(i) + ": " + str(x))
+                strength = strength + i * x
+            if self.data[i] != None:
+                x = x + self.data[i]
+            
+
+        return strength
 
     def run2(self):
         return ""
