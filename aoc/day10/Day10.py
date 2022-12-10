5783050a6fb46b2f6494f7c3496859a1320ad868
Martin <github@msturm.net>
2022-12-10 15:16:13 +0100

    Add day10 part2

--- aoc/day10/Day10.py
+++ aoc/day10/Day10.py
@@ -6,7 +6,7 @@ class Day10(Day):
     use_dummy = True
 
     def convert(self, lines):
-        ops = [ None ]
+        ops = []
         for line in lines:
             ops.append(None)
             if line != "noop":
@@ -16,15 +16,25 @@ class Day10(Day):
     def run1(self):
         x = 1
         strength = 0
-        for i in range(1, 221):
-            if i == 20 or i == 60 or i == 100  or i == 140  or i == 180  or i == 220:
-                print(str(i) + ": " + str(x))
-                strength = strength + i * x
+        for i in range(220):
+            cycle = i + 1
+            if cycle == 20 or cycle == 60 or cycle == 100  or cycle == 140  or cycle == 180  or cycle == 220:
+                strength = strength + cycle * x
             if self.data[i] != None:
                 x = x + self.data[i]
-            
-
         return strength
 
     def run2(self):
-        return ""
+        sprite = 1
+        screen = ""
+        for i in range(240):
+            crt = i % 40
+            if crt == 0:
+                screen = screen + "\n"
+            if abs(sprite - crt) <= 1:
+                screen = screen + "#"
+            else:
+                screen = screen + "."
+            if self.data[i] != None:
+                sprite = sprite + self.data[i]
+        return screen
--- test/test_Day10.py
+++ test/test_Day10.py
@@ -5,19 +5,21 @@ from aoc.day10.Day10 import Day10
 class testDay10(unittest.TestCase):
     def test_part1_dummy(self):
         class_under_test = Day10().init_with(True)
-        self.assertEqual("", str(class_under_test.run1()))
+        self.assertEqual("13140", str(class_under_test.run1()))
 
     def test_part1(self):
         class_under_test = Day10().init_with(False)
-        self.assertEqual("", str(class_under_test.run1()))
+        self.assertEqual("12520", str(class_under_test.run1()))
 
     def test_part2_dummy(self):
+        expected = "\n##..##..##..##..##..##..##..##..##..##..\n###...###...###...###...###...###...###.\n####....####....####....####....####....\n#####.....#####.....#####.....#####.....\n######......######......######......####\n#######.......#######.......#######....."
         class_under_test = Day10().init_with(True)
-        self.assertEqual("", str(class_under_test.run2()))
+        self.assertEqual(expected, str(class_under_test.run2()))
 
     def test_part2(self):
+        expected = "\n####.#..#.###..####.###....##..##..#....\n#....#..#.#..#....#.#..#....#.#..#.#....\n###..####.#..#...#..#..#....#.#....#....\n#....#..#.###...#...###.....#.#.##.#....\n#....#..#.#....#....#....#..#.#..#.#....\n####.#..#.#....####.#.....##...###.####."
         class_under_test = Day10().init_with(False)
-        self.assertEqual("", str(class_under_test.run2()))
+        self.assertEqual(expected, str(class_under_test.run2()))
 
 
 if __name__ == '__main__':
